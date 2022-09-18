1808aa2725b390742f05e738e5f9aa82ba88e27d
Cesar Osvaldo Oliveira Monteiro <cosvaldo@c1r8s4.42lisboa.com>
2022-09-18 18:47:07 +0100

    tests.c argv 8

--- tests.c
+++ tests.c
@@ -3,4 +3,6 @@
 ./a.out "4 3 2 1 1 2 3 4 4 3 2 1 1 2 3 4"; // true ?
 ./a.out "3 1 2 2 1 3 2 2 2 1 3 2 2 2 1 3"; // true 
 ./a.out "3 2 2 1 2 1 3 2 3 2 1 2 1 2 3 2"; // true 
-./a.out "1 2 2 3 2 3 1 2 1 2 3 2 3 2 1 2"; // true reverso do anterior 
+./a.out "1 2 2 3 2 3 1 2 1 2 3 2 3 2 1 2"; // true reverso horizontal do anterior 
+./a.out "2 2 3 1 2 2 1 3 3 1 2 2 1 2 3 2"; // true 
+./a.out "2 2 1 3 2 2 3 1 2 2 1 3 2 3 2 1"; // true reverso vertical do anterior
